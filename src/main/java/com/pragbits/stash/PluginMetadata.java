74e77d35ccdd0bbdc69a37d8db137d7bd9267f88
trigged <trigged@live.cn>
2015-04-29 19:02:20 +0800

    修改包名

--- src/main/java/com/pragbits/stash/PluginMetadata.java
+++ src/main/java/com/pragbits/stash/PluginMetadata.java
@@ -3,7 +3,7 @@ package com.pragbits.stash;
 public class PluginMetadata {
 
     public static String getPluginKey() {
-        return "com.pragbits.stash.stash2slack";
+        return "com.pubuim.stash.stash2pubu";
     }
 
     public static String getCompleteModuleKey(String moduleKey) {
